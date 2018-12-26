f592924d2325412c2c1dda03f41e1af711014f0e
Anarawd <kgursyn@gmail.com>
2018-12-26 17:30:22 +0100

    Create SUMA.cpp

---
+++ SUMA -Suma/SUMA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,suma=0;
+    while(cin>>a)
+    {
+        suma=suma+a;
+        cout<<suma<<endl;
+    }
+    return 0;
+}
+
+
