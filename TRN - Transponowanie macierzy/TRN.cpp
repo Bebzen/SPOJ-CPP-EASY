c51a646171ba7a53a10169f86b44384ef1ce5d51
Anarawd <kgursyn@gmail.com>
2018-12-26 17:48:52 +0100

    Create TRN.cpp

---
+++ TRN - Transponowanie macierzy/TRN.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    int macierz[m][n];
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cin>>macierz[i][j];
+        }
+    }
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<m;j++)
+        {
+            cout<<macierz[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
