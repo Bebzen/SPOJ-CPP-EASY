a0428d7878771f0f23cb09a3c2d95cbb4c6cdf8d
Anarawd <kgursyn@gmail.com>
2018-12-26 17:51:14 +0100

    Create MWPZ06A.cpp

---
+++ MWPZ06A - Ciążowy specjalista/MWPZ06A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    double X,Y,Z,a;
+    for (int i=0;i<t;i++)
+    {
+        cin>>X>>Y>>Z;
+        a = ((Z*Y-X-Y)/(1-Z))*(-12);
+        cout<<fixed<<setprecision(0)<<a<<endl;
+    }
+    return 0;
+}
