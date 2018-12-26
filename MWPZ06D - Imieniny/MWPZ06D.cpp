1b9422bd80570634a6ec925224ee25fd3604e551
Anarawd <kgursyn@gmail.com>
2018-12-26 17:51:12 +0100

    Create MWPZ06D.cpp

---
+++ MWPZ06D - Imieniny/MWPZ06D.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    int x,y;
+    for (int i=0;i<t;i++)
+    {
+        cin>>x>>y;
+        if(x==1 && y>0) cout<<"TAK"<<endl;
+        else if(((x-1)>y) || (y%(x-1)>0))  cout<<"TAK"<<endl;
+        else cout<<"NIE"<<endl;
+    }
+    return 0;
+}
