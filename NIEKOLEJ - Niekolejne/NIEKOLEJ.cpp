fe2a5a100316cbb65ff657c36a5855ddd7719fb7
Anarawd <kgursyn@gmail.com>
2018-12-27 19:05:02 +0100

    Create NIEKOLEJ.cpp

---
+++ NIEKOLEJ - Niekolejne/NIEKOLEJ.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    if(t<3 && t!=0)
+    cout<<"NIE"<<endl;
+    else
+    {
+        int tab[t+1];
+        for(int i=0;i<=t;i++)
+        {
+            tab[i]=i;
+        }
+        for(int i=1;i<t+1;i+=2)
+        {
+            cout<<tab[i]<<" ";
+        }
+        for(int i=0;i<t+1;i+=2)
+        {
+            cout<<tab[i]<<" ";
+        }
+    }
+    return 0;
+}
