31c0d5645369c061bc257e1be2f0257b806f9987
Anarawd <kgursyn@gmail.com>
2019-01-02 20:24:30 +0100

    Update XIWTPZA.cpp

--- XIWTPZA - Prostokąty/XIWTPZA.cpp
+++ XIWTPZA - Prostokąty/XIWTPZA.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int main()
 {
-    string a,b,c;
-    int d=0;
-    cin>>a>>b>>c;
-    for (int j=0;j<a.length();j++)
-    {
-        if (a[i]!= b[i]) d++;
-        if (a[i]!=c[i]&&c[i]== b[i]) d++;
-        if (b[i]==a[i]&&c[i]!= b[i]) d++;
-    }
-    cout<<d;
-    return 0;
+	double m,a,b,p,q,e;
+	cin>>m;
+	for (int i=0; i<m; i++)
+	{
+	    cin>>a>>b>>p>>q;
+        if (b>a){e=a;a=b;b=e;};
+        if (q>p) {e=p; p=q; q=e;};
+        if ((p<a&&q<b)||(p>a&&b>(2*p*q*a+(p*p-q*q)*sqrt(p*p+q*q-a*a))/(p*p+q*q))) cout<<"TAK"<<endl;
+        else cout<<"NIE"<<endl;
+	}
+	return 0;
 }
