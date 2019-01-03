5138d8bb1d6ede0431e2485de10bd0b27505607f
Anarawd <kgursyn@gmail.com>
2019-01-03 17:11:04 +0100

    Update KC008.cpp

--- KC008 - Sumy wielokrotne/KC008.cpp
+++ KC008 - Sumy wielokrotne/KC008.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int a;
-    int suma=0,suma2=0;
+    long long int suma=0,suma2=0;
     while (cin>>a)
     {
         if (a==0) {cout<<suma<<endl; suma2=suma2+suma; suma=0;}
